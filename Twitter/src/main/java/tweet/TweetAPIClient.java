11992db5e69fa17b2b5c35fdbbf48f5e0262cff3
Oussama0115 <ounnouchoussama@gmail.com>
2021-04-10 14:40:01 -0400

    updated

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -14,6 +14,28 @@ public class TweetAPIClient extends RestAPI {
     private final String CREATE_TWEET_ENDPOINT="/statuses/update.json";
     private final String DELETE_TWEET_ENDPOINT="/statuses/destroy.json";
     private final String GET_USER_TWEET_ENDPOINT="/statuses/home_timeline.json";
+    private final String GET_TIMELINE_TWEET_ENDPOINT="/statuses/user_timeline.json";
+    private final String CREATE_FAVORITE_ENDPOINT="/favorites/create.json";
+    private final String DESTROY_FAVORITE_ENDPOINT="/favorites/destroy.json";
+    private final String CREATE_RETWEET_ENDPOINT="/statuses/retweet.json";
+    private final String GET_LOOKUP_ENDPOINT = "/statuses/lookup.json";
+    private final String DELETE_MESSAGE_ENDPOINT = "/direct_messages/events/destroy.json";
+    private final String GET_USER_TIMELINE_ENDPOINT = "/statuses/user_timeline.json";
+    private final String CREATE_LISTS_ENDPOINT = "/lists/create.json";
+    private final String GET_LISTS_ENDPOINT = "/lists/list.json";
+    private final String CREATE_COLLECTIONS_ENDPOINT = "/collections/create.json";
+    private final String GET_COLLECTIONS_SHOW_ENDPOINT = "/collections/show.json";
+    private final String DESTROY_COLLECTIONS_ENDPOINT = "/collections/destroy.json";
+    private final String SEARCH_TWEET_ENDPOINT = "/search/tweets.json";
+    private final String GET_LIST_MEMBERS_ENDPOINT = "/lists/members.json";
+    private final String VERIFY_CREDENTIALS_ENDPOINT = "/account/verify_credentials.json";
+    private final String GET_PROFILE_BANNER_ENDPOINT = "/users/profile_banner.json";
+    private final String REMOVE_PROFILE_BANNER_ENDPOINT = "/account/remove_profile_banner.json";
+    private final String POST_SEARCH_ENDPOINT = "/saved_searches/create.json";
+    private final String DESTROY_SEARCH_ENDPOINT = "/saved_searches/destroy/:id.json";
+    private final String GET_LIST_SUBSCRIBERS_ENDPOINT = "/lists/subscribers.json";
+    private final String GET_FRIENDS_LIST_ENDPOINT = "/friends/list.json";
+    private final String GET_CUSTOM_PROFILES_LIST_ENDPOINT = "/custom_profiles/list.json";
 
     // GET all Tweet Information
     public ValidatableResponse getUserTimeTweet(){
@@ -63,16 +85,148 @@ public class TweetAPIClient extends RestAPI {
         String createdAt=pathEvaluator.get("id");
         System.out.println(createdAt);
     }
+    // GEt time line  Tweet Information
+    public ValidatableResponse getUserTimeLineTweet(){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_TIMELINE_TWEET_ENDPOINT).then().statusCode(200);
+    }
+    // Create favorites
+    public ValidatableResponse createFavorites(Long tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("id", tweetId)
+                .when().post(this.baseUrl + this.CREATE_FAVORITE_ENDPOINT)
+                .then();
 
+    }
+// Delete favorites
+public ValidatableResponse destroyFavorites(Long tweetId) {
+    return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+            .param("id", tweetId)
+            .when().post(this.baseUrl + this.DESTROY_FAVORITE_ENDPOINT)
+            .then();
 
-    // OAuth
-    // https://www.programcreek.com/java-api-examples/?api=com.github.scribejava.core.model.OAuthRequest
+    }
+// Retweet by using ID
+public ValidatableResponse retweet(Long tweetId) {
+    return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+            .param("id", tweetId)
+            .when().post(this.baseUrl + this.CREATE_RETWEET_ENDPOINT)
+            .then();
+}
+    // Get Lookup tweets
+    public ValidatableResponse lookupTweets(Long tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("id", tweetId)
+                .when().get(this.baseUrl + this.GET_LOOKUP_ENDPOINT)
+                .then();
+    }
 
+    // Delete a Tweet from user twitter
+    public ValidatableResponse deleteMessage(Long tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().delete(this.baseUrl + this.DELETE_MESSAGE_ENDPOINT)
+                .then();
+    }
+    // Create a List
+    public ValidatableResponse createList(String name) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("name", name)
+                .when().post(this.baseUrl + this.CREATE_LISTS_ENDPOINT)
+                .then();
+    }
 
+    // GET lists
+    public ValidatableResponse getLists() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_LISTS_ENDPOINT).then().statusCode(200);
+    }
 
-// https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/overview
+    // Create a collections
+    public ValidatableResponse createCollections(String name) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("name", name)
+                .when().post(this.baseUrl + this.CREATE_COLLECTIONS_ENDPOINT)
+                .then();
+    }
 
-    // https://api.twitter.com/1.1/statuses/update.json
+    public ValidatableResponse getCollections(String customId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("id", customId)
+                .when().get(this.baseUrl + this.GET_COLLECTIONS_SHOW_ENDPOINT)
+                .then();
+    }
+
+    // Delete collections
+    public ValidatableResponse destroyCollections(String customId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("id", customId)
+                .when().post(this.baseUrl + this.DESTROY_COLLECTIONS_ENDPOINT)
+                .then();
+    }
+    // Search tweet
+    public ValidatableResponse searchTweet(String value) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("q", value)
+                .when().get(this.baseUrl + this.SEARCH_TWEET_ENDPOINT).then().statusCode(200);
+    }
+    // Get list members
+    public ValidatableResponse getListMembers(long id) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("list_id", id)
+                .when().get(this.baseUrl + this.GET_LIST_MEMBERS_ENDPOINT).then().statusCode(200);
+    }
+    // Verify credentials
+    public ValidatableResponse verifyCredentials() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.VERIFY_CREDENTIALS_ENDPOINT).then().statusCode(200);
+    }
+
+    // Get profile banner
+    public ValidatableResponse getProfileBanner() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_PROFILE_BANNER_ENDPOINT).then().statusCode(404);
+    }
+
+    // Delete a profile banner
+    public ValidatableResponse deleteProfileBanner() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().post(this.baseUrl + this.REMOVE_PROFILE_BANNER_ENDPOINT).then().statusCode(200);
+    }
+
+    // Create search
+    public ValidatableResponse createSearch(String query) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("query", query)
+                .when().post(this.baseUrl + this.POST_SEARCH_ENDPOINT)
+                .then();
+    }
+
+    // Delete a search
+    public ValidatableResponse deleteSearch(long searchId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", searchId)
+                .when().post(this.baseUrl + this.DESTROY_SEARCH_ENDPOINT).then();
+    }
+
+    // Get list subscribers
+    public ValidatableResponse getSubscribers(long listId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", listId)
+                .when().get(this.baseUrl + this.GET_LIST_SUBSCRIBERS_ENDPOINT).then().statusCode(400);
+    }
+
+    // Get friends list
+    public ValidatableResponse getFriendsList() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_FRIENDS_LIST_ENDPOINT).then();
+    }
+    // Get custom profiles
+    public ValidatableResponse getCustomProfilesList() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_CUSTOM_PROFILES_LIST_ENDPOINT).then();
+    }
+
 
 
 }
--- Twitter/src/test/java/tweettest/TweetAPIClientTest.java
+++ Twitter/src/test/java/tweettest/TweetAPIClientTest.java
@@ -8,6 +8,8 @@ import tweet.TweetAPIClient;
 
 import java.util.UUID;
 
+import static io.restassured.RestAssured.given;
+
 public class TweetAPIClientTest {
 
     private TweetAPIClient tweetAPIClient;
@@ -23,16 +25,16 @@ public class TweetAPIClientTest {
     @Test
     public void testUserCanTweetSuccessfully() {
         // User sent a tweet tweet
-        String tweet = "We are learning Rest API using Rest Assured and our First Tweet"+ UUID.randomUUID().toString();
+        String tweet = "I am learning Rest API using Rest Assured and our First Tweet" + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-       // System.out.println(response.extract().body().asPrettyString());
+        // System.out.println(response.extract().body().asPrettyString());
         // Verify that the tweet is successful
         response.statusCode(200);
         // Verity tweet value
-        String actualTweet=response.extract().body().path("text");
-       // Long id= response.extract().body().path("id");
+        String actualTweet = response.extract().body().path("text");
+        // Long id= response.extract().body().path("id");
         //System.out.println(id);
-        Assert.assertEquals(actualTweet,tweet,"Tweet is not match");
+        Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
     }
 
     @Test
@@ -41,23 +43,23 @@ public class TweetAPIClientTest {
         String tweet = "We are learning Rest API using Rest Assured and our First Tweet Same Tweet";
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         // Verify that the tweet is successful
-       response.statusCode(403);
+        response.statusCode(403);
         // Verity Retweet
         System.out.println(response.extract().body().asPrettyString());
-        String expectedMessage="Status is a duplicate.";
-        String actualTweet=response.extract().body().path("errors[0].message");
-        Assert.assertEquals(actualTweet,expectedMessage,"Tweet is match");
-        Assert.assertNotEquals("403",200);
+        String expectedMessage = "Status is a duplicate.";
+        String actualTweet = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualTweet, expectedMessage, "Tweet is match");
+        Assert.assertNotEquals("403", 200);
     }
 
 
     @Test
-    public void testDeleteTweet(){
-        String tweet="We are learning Rest API using Rest Assured and our First Tweet82d120dd-9045-44f3-a3c9-8720409fae20";
-        ValidatableResponse deleteResponse= this.tweetAPIClient.deleteTweet(1378590700409921541L);
+    public void testDeleteTweet() {
+        String tweet = "I am learning Rest API using Rest Assured and our First Tweetbe9d71fe-6de8-4709-b1c2-84a677bb6aab";
+        ValidatableResponse deleteResponse = this.tweetAPIClient.deleteTweet(1379296713744265216L);
         deleteResponse.statusCode(200);
-        String actualTweet= deleteResponse.extract().body().path("text");
-        Assert.assertEquals(tweet,actualTweet);
+        String actualTweet = deleteResponse.extract().body().path("text");
+        Assert.assertEquals(tweet, actualTweet);
 
     }
 
@@ -66,6 +68,7 @@ public class TweetAPIClientTest {
     public void testResponseTime() {
         ValidatableResponse response = this.tweetAPIClient.responseTime();
     }
+
     @Test(enabled = false)
     public void testHeaderValue() {
         this.tweetAPIClient.headerValue();
@@ -87,6 +90,210 @@ public class TweetAPIClientTest {
         //String actualTweet = response.extract().body().path("text");
         //Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
     }
+@Test
+    public void  TestGetUserTimeLineTweet() {
+        ValidatableResponse response = this.tweetAPIClient.getUserTimeLineTweet();
+        System.out.println(response.extract().body().asPrettyString());
+     String expectedTweet = "We are learning Rest API using Rest Assured and our First Tweet Same Tweet";
+      String actualTweet = response.extract().body().path("[0].text");
+      Assert.assertEquals(expectedTweet, actualTweet);
+    }
+    @Test(enabled = true)
+    public void TestCreateFavorites() {
+        String tweet = "This is the first time I tweet using Assured Rest API";
+        ValidatableResponse response = this.tweetAPIClient.createFavorites(1379927918281900034L);
+       //response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualTweet = response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,tweet, "Tweet did not match");
+
+    }
+    @Test(enabled = true)
+    public void TestDestroyFavorites() {
+        String tweet = "This is the first time I tweet using Assured Rest API";
+        ValidatableResponse response = this.tweetAPIClient.destroyFavorites(1379927918281900034L);
+        //deleteResponse.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualTweet = response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,tweet, "Tweet did not match");
+
+    }
+    @Test(enabled = true)
+    public void TestRetweet() {
+        String retweet = "RT @OOunnouch: This is the first time I tweet using Assured Rest API";
+        ValidatableResponse response = this.tweetAPIClient.retweet(1379927918281900034L);
+        //deleteResponse.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualTweet = response.extract().body().path("text");
+        Assert.assertEquals(actualTweet, retweet);
+
+    }
+    @Test
+    public void TestUserCannotRetweetTwice() {
+        String retweet = "You have already retweeted this Tweet.";
+        ValidatableResponse response = this.tweetAPIClient.retweet(1379927918281900034L);
+        //deleteResponse.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualTweet = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualTweet, retweet);
+    }
+    @Test
+    public void testLookupTweets() {
+        String tweet = "This is the first time I tweet using Assured Rest API";
+        ValidatableResponse response = this.tweetAPIClient.lookupTweets(1379927918281900034l);
+        //response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualTweet = response.extract().body().path("[0].text");
+        Assert.assertEquals(actualTweet, tweet, "Tweet did not match");
+    }
+    @Test
+    public void testUserCannotDeleteMessage() {
+        String message = "This application is not allowed to access or delete your direct messages.";
+        ValidatableResponse response = this.tweetAPIClient.deleteMessage(1380651552638128131l);
+        //response.statusCode(403);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualMessage = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualMessage, message, "Message did not match");
+    }
+    @Test
+    public void testCreateList() {
+        String name = "my first list created";
+        ValidatableResponse response = this.tweetAPIClient.createList(name);
+        //response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualName = response.extract().body().path("name");
+        Assert.assertEquals(actualName, name, "Tweet did not match");
+    }
+    @Test
+    public void testGetLists() {
+        String name = "my first list created";
+        ValidatableResponse response = this.tweetAPIClient.getLists();
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualName = response.extract().body().path("[0].name");
+        Assert.assertEquals(actualName, name, "Tweet did not match");
+    }
+    @Test
+    public void testCreateCollections() {
+        String name = "my first collection";
+        ValidatableResponse response = this.tweetAPIClient.createCollections(name);
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testUserCannotGetCollections() {
+        ValidatableResponse response = this.tweetAPIClient.getCollections("custom-1379482328825802760");
+        System.out.println(response.extract().body().asPrettyString());
+        String error = "Forbidden from viewing this collection";
+        String actualError = response.extract().body().path("error");
+        Assert.assertEquals(actualError, error, "Tweet did not match");
+    }
+
+    @Test
+    public void testDestroyCollections() {
+        ValidatableResponse response = this.tweetAPIClient.destroyCollections("custom-1379482328825802760");
+        System.out.println(response.extract().body().asPrettyString());
+        boolean actualTweet = response.extract().body().path("destroyed");
+        Assert.assertEquals(actualTweet, true, "Tweet did not match");
+    }
+    @Test
+    public void testSearchTweet() {
+        String text = "RT @OOunnouch: I am learning Rest API using Rest Assured and my First Tweet Same Tweet";
+        ValidatableResponse response = this.tweetAPIClient.searchTweet("@OOunnouch");
+        //response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualText = response.extract().body().path("statuses[0].text");
+        Assert.assertEquals(actualText, text, "Tweet did not match");
+    }
+    @Test
+    public void testGetListMembers() {
+        String user = null;
+        ValidatableResponse response = this.tweetAPIClient.getListMembers(1380651552638128131l);
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualText = response.extract().body().path("[0].next_cursor");
+        Assert.assertEquals(actualText, user, "Tweet did not match");
+    }
+    @Test
+    public void testVerifyCredentials() {
+        String expectedName = "Oussama Ounnouch";
+        ValidatableResponse response = this.tweetAPIClient.verifyCredentials();
+        System.out.println(response.extract().body().asPrettyString());
+        String actualName = response.extract().body().path("name");
+        Assert.assertEquals(actualName, expectedName, "Name did not not match");
+    }
+
+    @Test
+    public void testGetProfileBanner() {
+        String expectedError = "Sorry, that page does not exist.";
+        ValidatableResponse response = this.tweetAPIClient.getProfileBanner();
+        System.out.println(response.extract().body().asPrettyString());
+        String actualError = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualError, expectedError, "Error did not not match");
+    }
+
+    @Test
+    public void testDeleteProfileBanner() {
+        ValidatableResponse response = this.tweetAPIClient.deleteProfileBanner();
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testCreateSearch() {
+        String query = "Lyes pnt1234";
+        ValidatableResponse response = this.tweetAPIClient.createSearch(query);
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualQuery = response.extract().body().path("query");
+        Assert.assertEquals(actualQuery, query, "Query did not match");
+    }
+
+    @Test
+    public void testDeleteSearch() {
+        String name = null;
+        ValidatableResponse response = this.tweetAPIClient.deleteSearch(1379578368178405377l);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualQuery = response.extract().body().path("name");
+        Assert.assertEquals(actualQuery, name, "Query did not match");
+    }
+
+    @Test
+    public void testCannotDeleteSearchTwice() {
+        String name = "Sorry, that page does not exist.";
+        ValidatableResponse response = this.tweetAPIClient.deleteSearch(1379578368178405377l);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualQuery = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualQuery, name, "Query did not match");
+    }
+
+    @Test
+    public void testGetSubscribers() {
+        String expectedError = "You must specify either a list ID or a slug and owner.";
+        ValidatableResponse response = this.tweetAPIClient.getSubscribers(1379494425441804297l);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualError = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualError, expectedError, "Error did not not match");
+    }
+
+    @Test
+    public void testGetFriendsList() {
+        String name = "Lyes Khemouche";
+        ValidatableResponse response = this.tweetAPIClient.getFriendsList();
+        System.out.println(response.extract().body().asPrettyString());
+        String actualName = response.extract().body().path("users[0].name");
+        Assert.assertEquals(actualName, name, "Error did not not match");
+    }
+    @Test
+    public void testCannotGetCustomProfilesList() {
+        String name = "Client is not permitted to perform this action.";
+        ValidatableResponse response = this.tweetAPIClient.getCustomProfilesList();
+        System.out.println(response.extract().body().asPrettyString());
+        String actualQuery = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualQuery, name, "Query did not match");
+    }
+
+
 
 
 
