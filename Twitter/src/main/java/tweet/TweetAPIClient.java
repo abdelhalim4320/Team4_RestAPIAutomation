c70fb6c81ee5815be65fff835fe0ae5217eb1bc1
lamiBra <lamias640@gmail.com>
2021-04-10 13:45:49 -0400

    last push

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -1,10 +1,13 @@
 package tweet;
 
 import base.RestAPI;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
@@ -14,6 +17,29 @@ public class TweetAPIClient extends RestAPI {
     private final String CREATE_TWEET_ENDPOINT="/statuses/update.json";
     private final String DELETE_TWEET_ENDPOINT="/statuses/destroy.json";
     private final String GET_USER_TWEET_ENDPOINT="/statuses/home_timeline.json";
+    private final String GET_STATUSES_HOME_TIMELINE="/statuses/home_timeline.json";
+    private final String POST_FAVORITE_TWEET_ENDPOINT="/favorites/create.json";
+    private final String POST_DELETE_FAVORITE = "/favorites/destroy.json";
+    private final String POST_RETWEET_ENDPOINT="/statuses/retweet.json";
+    private final String POST_UNRETWEET_ENDPOINT="/unretweet.json";
+    private final String GET_STATUSES_MENTIONS_TIMELINE="/mentions_timeline.json";
+    private final String GET_LISTS_MEMBERS="/lists/members/show.json";
+    private final String POST_DIRECT_MESSAGE="/direct_messages/events/new.json";
+    private final String DELETE_DIRECT_MESSAGE="/direct_messages/events/destroy.json";
+    private final String GET_FREIND_LIST_ENDPOINT="/friends/list.json";
+    private final String POST_LISTS_ENDPOINT="/lists/create.json";
+    private final String DELETE_LISTS_ENDPOINT="/lists/destroy.json";
+    private final String POST_LISTS_MEMBERS_ENDPOINT="/lists/members/create.json";
+    private final String POST_LISTS_SUBSCRIBERS_ENDPOINT="/subscribers/create.json";
+    private final String GET_LISTS_SUBSCRIBERS_ENDPOINT="/lists/subscribers.json";
+    private final String GET_LISTS_SHOW_ENDPOINT="/lists/show.json";
+    private final String DESTROY_Members_ENDPOINT= "/members/destroy_all.json";
+    private final String POST_STATUES_ENDPOINT = "/statuses/update.json";
+    private final String GET_STATUES_ENDPOINT = "/statuses/show/";
+    private final String DELETE_STATUES_ENDPOINT = "/statuses/destroy/";
+    private final String POST_SAVE_SEARCH = "/saved_searches/list.json";
+
+
 
     // GET all Tweet Information
     public ValidatableResponse getUserTimeTweet(){
@@ -58,21 +84,139 @@ public class TweetAPIClient extends RestAPI {
 
     public  void checkProperty(){
         Response response= given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
-                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT);
+       .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT);
         JsonPath pathEvaluator= response.jsonPath();
         String createdAt=pathEvaluator.get("id");
         System.out.println(createdAt);
     }
 
+    public ValidatableResponse getTimeline(){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_STATUSES_HOME_TIMELINE).then().statusCode(200);
+    }
+public ValidatableResponse getTimeline1(){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_STATUSES_MENTIONS_TIMELINE).then().statusCode(200);
+    }
 
-    // OAuth
-    // https://www.programcreek.com/java-api-examples/?api=com.github.scribejava.core.model.OAuthRequest
+
+    public ValidatableResponse postFavorite(long tweetId){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .param("id",tweetId)
+                .when().post(this.baseUrl+this.POST_FAVORITE_TWEET_ENDPOINT + tweetId + ".Json")
+                .then();
+    }
+
+    public ValidatableResponse postDeleteFavorite(long tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().post(this.baseUrl + this.POST_DELETE_FAVORITE+ tweetId + ".Json")
+                .then();
+    }
+
+    public ValidatableResponse postRetweet(long id){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .param("id",id)
+                .when().post(this.baseUrl+this.POST_RETWEET_ENDPOINT)
+                .then();
+    }
+    public ValidatableResponse postUnRetweet(long id){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .queryParam("id",id)
+                .when().post(this.baseUrl+this.POST_UNRETWEET_ENDPOINT)
+                .then();
+    }
 
 
+//    ========================================== send message
+    public ValidatableResponse postDirectMessages() throws FileNotFoundException {
+    FileInputStream jsonmessage=new FileInputStream("C:/Users/user/IdeaProjects/Team4_RestAPIAutomation/Twitter/src/main/jsonDirectory/jsonMessage.json");
+    return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+            //.log().all()
+            .accept(ContentType.JSON)
+            .header("content-type", "application/json")
+            .contentType(ContentType.JSON)
+            .body(jsonmessage)
+            .when().post(this.baseUrl + this.POST_DIRECT_MESSAGE)
+            .then();//.log().all();
+}
+//=======================================get list
+    public ValidatableResponse getFreindList(String screenName){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+            //.queryParam("screen_Name",screenName)
+            .when().get(this.baseUrl+this.GET_FREIND_LIST_ENDPOINT)
+            .then();
+}
+//===============================create list
+    public ValidatableResponse creatFreindList(String screenName){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+            .queryParam("list",screenName)
+            .when().post(this.baseUrl+this.POST_LISTS_ENDPOINT)
+            .then();
+}
+//=========================delete list
+public ValidatableResponse deleteMemeberlIST(long listId){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+            .queryParam("id",listId)
+            .when().post(this.baseUrl+this.DELETE_LISTS_ENDPOINT).then().statusCode(200);
+}
 
-// https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/overview
 
-    // https://api.twitter.com/1.1/statuses/update.json
+    public ValidatableResponse creatListMember(long listId, String screenName){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+            .queryParam("ID", listId)
+             .queryParam("name", screenName)
+            .when().post(this.baseUrl+this.POST_LISTS_MEMBERS_ENDPOINT)
+            .then();
+}
+//=============================================
+    public ValidatableResponse creatListSubscriber(long listID){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+            .queryParam("id",listID)
+            .when().post(this.baseUrl+this.POST_LISTS_SUBSCRIBERS_ENDPOINT)
+            .then();
+}
+    public ValidatableResponse getListSubscriber(long listID){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+            .queryParam("id",listID)
+            .when().get(this.baseUrl+this.GET_LISTS_SUBSCRIBERS_ENDPOINT)
+            .then();
+}
+    public ValidatableResponse getListShow(long listID){
+    return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+            .queryParam("id",listID)
+            .when().get(this.baseUrl+this.GET_LISTS_SHOW_ENDPOINT)
+            .then();
+}
+    public ValidatableResponse destroyMembersAll(long listId, String slug){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+                .queryParam("id",listId)
+                .queryParam("Slug", slug)
+                .when().post(this.baseUrl+this.DESTROY_Members_ENDPOINT+ listId + slug + ".Json")
+                .then();
+    }
+//    ==========================
+    public ValidatableResponse postStatus(String Status) {
+    return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+            .queryParam("status", Status)
+            .when().post(this.baseUrl + this.POST_STATUES_ENDPOINT)
+            .then();
+}
+    public ValidatableResponse getStatues(String postId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", postId)
+                .when().get(this.baseUrl + this.GET_STATUES_ENDPOINT + postId + ".Json")
+                .then();
+    }
+
+    public ValidatableResponse deleteStatus(String postId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", postId)
+                .when().post(this.baseUrl + this.DELETE_STATUES_ENDPOINT + postId + ".Json")
+                .then();
+    }
+
+
 
 
 }
--- Twitter/src/test/java/tweettest/TweetAPIClientTest.java
+++ Twitter/src/test/java/tweettest/TweetAPIClientTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import tweet.TweetAPIClient;
 
+import java.io.FileNotFoundException;
 import java.util.UUID;
 
 public class TweetAPIClientTest {
@@ -23,55 +24,71 @@ public class TweetAPIClientTest {
     @Test
     public void testUserCanTweetSuccessfully() {
         // User sent a tweet tweet
-        String tweet = "We are learning Rest API using Rest Assured and our First Tweet"+ UUID.randomUUID().toString();
+        String tweet = "This is my first tweet" + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-       // System.out.println(response.extract().body().asPrettyString());
+        System.out.println(response.extract().body().asPrettyString());
         // Verify that the tweet is successful
         response.statusCode(200);
         // Verity tweet value
-        String actualTweet=response.extract().body().path("text");
-       // Long id= response.extract().body().path("id");
+        String actualTweet = response.extract().body().path("text");
+        // Long id= response.extract().body().path("id");
         //System.out.println(id);
-        Assert.assertEquals(actualTweet,tweet,"Tweet is not match");
+        Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
     }
 
     @Test
     public void testUserCanNotTweetTheSameTweetTwiceInARow() {
         // User sent a tweet
-        String tweet = "We are learning Rest API using Rest Assured and our First Tweet Same Tweet";
+        String tweet = "I'm learning Rest API using Rest Assured and my second Tweet Same Tweet";
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         // Verify that the tweet is successful
-       response.statusCode(403);
+        response.statusCode(403);
         // Verity Retweet
         System.out.println(response.extract().body().asPrettyString());
-        String expectedMessage="Status is a duplicate.";
-        String actualTweet=response.extract().body().path("errors[0].message");
-        Assert.assertEquals(actualTweet,expectedMessage,"Tweet is match");
-        Assert.assertNotEquals("403",200);
+        String expectedMessage = "Status is a duplicate.";
+        String actualTweet = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualTweet, expectedMessage, "Tweet is match");
+        Assert.assertNotEquals("403", 200);
+    }
+
+    @Test
+    public void testUserCanNotTweetTheSameTweetTwiceInARow2() {
+        // User sent a tweet
+        String tweet = "I'm learning Rest API using Rest Assured and my second Tweet Same Tweet";
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        // Verify that the tweet is successful
+        response.statusCode(403);
+        // Verity Retweet
+        System.out.println(response.extract().body().asPrettyString());
+        int expectedMessage = 187;
+        int actualTweet = response.extract().body().path("errors[0].code");
+        Assert.assertEquals(actualTweet, expectedMessage, "Tweet is match");
+        Assert.assertNotEquals("403", 200);
     }
 
 
     @Test
-    public void testDeleteTweet(){
-        String tweet="We are learning Rest API using Rest Assured and our First Tweet82d120dd-9045-44f3-a3c9-8720409fae20";
-        ValidatableResponse deleteResponse= this.tweetAPIClient.deleteTweet(1378590700409921541L);
+    public void testDeleteTweet() {
+        String tweet = "I'm learning Rest API using Rest Assured and my second Tweet Same Tweet";
+        ValidatableResponse deleteResponse = this.tweetAPIClient.deleteTweet(1379440056599990277L);
         deleteResponse.statusCode(200);
-        String actualTweet= deleteResponse.extract().body().path("text");
-        Assert.assertEquals(tweet,actualTweet);
+        String actualTweet = deleteResponse.extract().body().path("text");
+        Assert.assertEquals(tweet, actualTweet);
 
     }
 
 
-    @Test(enabled = false)
+    @Test
     public void testResponseTime() {
         ValidatableResponse response = this.tweetAPIClient.responseTime();
     }
-    @Test(enabled = false)
+
+    @Test
     public void testHeaderValue() {
         this.tweetAPIClient.headerValue();
     }
 
-    @Test(enabled = false)
+    @Test
     public void testPropertyFromResponse() {
         //1. User send a tweet
         String tweet = "We are learning Rest API Automation with WeekdaysEvening_Selenium_NY_Summer2020" + UUID.randomUUID().toString();
@@ -87,6 +104,168 @@ public class TweetAPIClientTest {
         //String actualTweet = response.extract().body().path("text");
         //Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
     }
+
+    @Test
+    public void getTimelineTest() {
+        String tweet = "This is my first tweetea6ad6e4-cd84-4302-b608-3de29005e684";
+        ValidatableResponse response = this.tweetAPIClient.getTimeline();
+        System.out.println(response.extract().body().asPrettyString());
+
+        response.statusCode(200);
+        String actualTweet = response.extract().body().path("text");
+        Assert.assertEquals(tweet, actualTweet);
+
+    } @Test
+    public void getTimelineTest1() {
+        String tweet = "This is my first tweet67233cb6-4042-4b21-b330-192177952926";
+        ValidatableResponse response = this.tweetAPIClient.getTimeline1();
+        System.out.println(response.extract().body().asPrettyString());
+
+//        response.statusCode(200);
+//        String actualTweet = response.extract().body().path("text");
+//        Assert.assertEquals(tweet, actualTweet);
+    }
+
+    @Test
+    public void testPostFavoriteTweet() {
+        ValidatableResponse response = this.tweetAPIClient.postFavorite(1379434456721674243l);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualList = response.extract().body().path("text");
+        Assert.assertEquals(actualList,"","screen name doesn't match");
+
+    }
+
+    @Test
+    public void testFavoriteDelete() {
+        ValidatableResponse response = this.tweetAPIClient.postDeleteFavorite(1379434456721674243l);
+        System.out.println(response.extract().body().asPrettyString());
+        String actualList = response.extract().body().path("text");
+        Assert.assertEquals(actualList,"","screen name doesn't match");
+
+
+    }
+
+    @Test
+    public void testPostUnReTweet() {
+        // User sent a tweet tweet
+        String unretweet="This is my first tweet67233cb6-4042-4b21-b330-192177952926";
+        ValidatableResponse response = this.tweetAPIClient.postUnRetweet(1379434456721674243l);
+        System.out.println(response.extract().body().asPrettyString());
+        //response.statusCode(200);
+        String actualTweet = response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,unretweet);
+    }
+
+
+//======================================
+
+    @Test
+    public void testPosMessage() throws FileNotFoundException {
+       // String message="";
+        ValidatableResponse response = this.tweetAPIClient.postDirectMessages();
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+//        String actualTweet = response.extract().body().path("text");
+//        Assert.assertEquals(actualTweet,message);
+    }
+//    ==============================================================get list
+    @Test
+    public void testGetFrendList(){
+    String screenName="DALAL";
+    ValidatableResponse response = this.tweetAPIClient.getFreindList(screenName);
+    System.out.println(response.extract().body().asPrettyString());
+   // String actualTweet = response.extract().body().path("");
+   // Assert.assertEquals(actualTweet,screenName);
+    }
+    @Test
+    public void testPostList(){
+    String screenName="lamia";
+    ValidatableResponse response = this.tweetAPIClient.creatFreindList(screenName);
+    System.out.println(response.extract().body().asPrettyString());
+        int expectedList = 85;
+        int actualList = response.extract().body().path("errors[0].code");
+     Assert.assertEquals(actualList,expectedList,"screen name doesn't match");
+
+        }
+    @Test
+    public void testDelete() {
+        String tweet = "DALAL";
+        ValidatableResponse deleteResponse = this.tweetAPIClient.deleteMemeberlIST(1380478276842446854l);
+        deleteResponse.statusCode(200);
+        String actualTweet = deleteResponse.extract().body().path("text");
+        Assert.assertEquals(tweet, actualTweet);
+
+    }
+        @Test
+    public void testPostListMember(){
+    String name= "Vice President Kamala Harris";
+    ValidatableResponse response = this.tweetAPIClient.creatListMember(1380553288358236164l, name);
+    System.out.println(response.extract().body().asPrettyString());
+//        int expectedList = 25;
+//        int actualList = response.extract().body().path("errors[0].code");
+//     Assert.assertEquals(actualList,expectedList,"screen name doesn't match");
+       }
+//=========================================
+    @Test
+    public void testPostListSubscriber(){
+    ValidatableResponse response = this.tweetAPIClient.creatListSubscriber(1380478276842446854l);
+    System.out.println(response.extract().body().asPrettyString());
+    int expectedList = 34;
+    int actualList = response.extract().body().path("errors[0].code");
+    Assert.assertEquals(actualList,expectedList,"screen name doesn't match");
+}
+    @Test
+    public void testGetListSubscriber(){
+    ValidatableResponse response = this.tweetAPIClient.getListSubscriber(1380478276842446854l);
+    System.out.println(response.extract().body().asPrettyString());
+    int expectedList = 112;
+    int actualList = response.extract().body().path("errors[0].code");
+    Assert.assertEquals(actualList,expectedList,"screen name doesn't match");
+}
+    @Test
+    public void testGetListShow(){
+    ValidatableResponse response = this.tweetAPIClient.getListShow(1380478276842446854l);
+    System.out.println(response.extract().body().asPrettyString());
+    int expectedList = 112;
+    int actualList = response.extract().body().path("errors[0].code");
+    Assert.assertEquals(actualList,expectedList,"screen name doesn't match");
+}
+    @Test
+    public void testDestroyMembersAll() {
+        String slug = "POTUS";
+        ValidatableResponse deleteResponse = this.tweetAPIClient.destroyMembersAll(1380553288358236164l,"POTUS");
+       // deleteResponse.statusCode(200);
+        String actualList = deleteResponse.extract().body().path("text");
+        Assert.assertEquals(actualList, slug,"screen name doesn't match");
+
+    }
+//    =========================
+@Test
+public void testPostStatus() {
+    ValidatableResponse response = this.tweetAPIClient.postStatus("Hi this is me, wish you a positive day!");
+    System.out.println(response.extract().body().asPrettyString());
+    String actualList = response.extract().body().path("text");
+    Assert.assertEquals(actualList,"Hi this is me, wish you a positive day!","screen name doesn't match");
+}
+
+    @Test
+    public void testGetStatus() {
+        ValidatableResponse response = this.tweetAPIClient.getStatues("1380889792762806275");
+        System.out.println(response.extract().body().asPrettyString());
+        String actualList = response.extract().body().path("text");
+        Assert.assertEquals(actualList,"Hi this is Lamia, have a positive day!","screen name doesn't match");
+    }
+
+    @Test
+    public void testDeleteStatus() {
+        ValidatableResponse response = this.tweetAPIClient.deleteStatus("1380889221167259651");
+        System.out.println(response.extract().body().asPrettyString());
+        String actualList = response.extract().body().path("text");
+        Assert.assertEquals(actualList,"Hi this is me, how are you?","screen name doesn't match");
+    }
+
+
+
 
 
 
